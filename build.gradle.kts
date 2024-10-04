32c77c9ed56cf16e766047b8bfcdb60cb1623716
Chockencore <sphillot@netplus.ch>
2024-10-04 18:00:20 +0200

    feat: add plugin org.sonarqube and configuration
    Add the base configuration to run the SonarCloud analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "BookswapEPFL_Bookswap")
+        property("sonar.organization", "bookswapepfl")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
