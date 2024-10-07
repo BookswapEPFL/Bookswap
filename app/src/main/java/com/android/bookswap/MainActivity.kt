c9007358f042ede098ea76a5aa9b4e5df9b2c4ba
matii <165163653+matiisalv@users.noreply.github.com>
2024-10-07 16:25:15 +0200

    fix (structure): refactor names of files to match project

--- app/src/main/java/com/android/bookswap/MainActivity.kt
+++ app/src/main/java/com/android/bookswap/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.android.sample
+package com.android.bookswap
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,8 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.sample.resources.C
-import com.android.sample.ui.theme.SampleAppTheme
+import com.android.bookswap.resources.C
+import com.android.bookswap.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
