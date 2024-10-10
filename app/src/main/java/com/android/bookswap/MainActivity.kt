87995b1bd7a756eaaaa7aacbd501b61cf6fb8da9
TheoSchlaeppi <81753102+TheoSchlaeppi@users.noreply.github.com>
2024-10-10 11:43:25 +0200

    chore: apply KTFmt formatting for the branch

--- app/src/main/java/com/android/bookswap/MainActivity.kt
+++ app/src/main/java/com/android/bookswap/MainActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,57 +21,42 @@ import com.android.bookswap.ui.navigation.Screen
 import com.android.bookswap.ui.theme.BookSwapAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BookSwapAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                        .semantics { testTag = C.Tag.main_screen_container },
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BookSwapApp()
-                }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      BookSwapAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
+            color = MaterialTheme.colorScheme.background) {
+              BookSwapApp()
             }
-        }
+      }
     }
+  }
 
+  @Composable
+  fun BookSwapApp() {
+    val navController = rememberNavController()
+    val navigationActions = NavigationActions(navController)
 
-    @Composable
-    fun BookSwapApp() {
-        val navController = rememberNavController()
-        val navigationActions = NavigationActions(navController)
-
-        NavHost(navController = navController, startDestination = Route.AUTH) {
-            navigation(
-                startDestination = Screen.AUTH,
-                route = Route.AUTH
-            ) {
-                composable(Screen.AUTH) {/*Todo*/ }
-            }
-            navigation(
-                startDestination = Screen.CHATLIST,
-                route = Route.CHAT
-            ) {
-                composable(Screen.CHATLIST) {/*Todo*/ }
-                composable(Screen.CHAT) {/*Todo*/ }
-            }
-            navigation(
-                startDestination = Screen.MAP,
-                route = Route.MAP
-            ) {
-                composable(Screen.MAP) {/*Todo*/ }
-            }
-            navigation(
-                startDestination = Screen.NEWBOOK,
-                route = Route.NEWBOOK
-            ) {
-                composable(Screen.NEWBOOK) {/*Todo*/ }
-                composable(Screen.ADD_BOOK_MANUALLY) {/*Todo*/ }
-                composable(Screen.ADD_BOOK_SCAN) {/*Todo*/ }
-                composable(Screen.ADD_BOOK_ISBN) {/*Todo*/ }
-            }
-        }
+    NavHost(navController = navController, startDestination = Route.AUTH) {
+      navigation(startDestination = Screen.AUTH, route = Route.AUTH) {
+        composable(Screen.AUTH) { /*Todo*/}
+      }
+      navigation(startDestination = Screen.CHATLIST, route = Route.CHAT) {
+        composable(Screen.CHATLIST) { /*Todo*/}
+        composable(Screen.CHAT) { /*Todo*/}
+      }
+      navigation(startDestination = Screen.MAP, route = Route.MAP) {
+        composable(Screen.MAP) { /*Todo*/}
+      }
+      navigation(startDestination = Screen.NEWBOOK, route = Route.NEWBOOK) {
+        composable(Screen.NEWBOOK) { /*Todo*/}
+        composable(Screen.ADD_BOOK_MANUALLY) { /*Todo*/}
+        composable(Screen.ADD_BOOK_SCAN) { /*Todo*/}
+        composable(Screen.ADD_BOOK_ISBN) { /*Todo*/}
+      }
     }
+  }
 }
--- app/src/test/java/com/android/bookswap/ui/navigation/NavigationActionsTest.kt
+++ app/src/test/java/com/android/bookswap/ui/navigation/NavigationActionsTest.kt
@@ -19,72 +19,70 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 
 public class NavigationActionsTest {
-    private lateinit var navigationDestination: NavDestination
-    private lateinit var navHostController: NavHostController
-    private lateinit var navigationActions: NavigationActions
+  private lateinit var navigationDestination: NavDestination
+  private lateinit var navHostController: NavHostController
+  private lateinit var navigationActions: NavigationActions
 
-    @Before
-    fun setUp() {
-        navigationDestination = mock(NavDestination::class.java)
-        navHostController = mock(NavHostController::class.java)
-        navigationActions = NavigationActions(navHostController)
-    }
+  @Before
+  fun setUp() {
+    navigationDestination = mock(NavDestination::class.java)
+    navHostController = mock(NavHostController::class.java)
+    navigationActions = NavigationActions(navHostController)
+  }
 
-    @Test
-    fun navigateToTopLevelDestination(){
-        navigationActions.navigateTo(TopLevelDestinations.CHAT)
-        verify(navHostController).navigate(eq(Route.CHAT), any<(NavOptionsBuilder) -> Unit>())
+  @Test
+  fun navigateToTopLevelDestination() {
+    navigationActions.navigateTo(TopLevelDestinations.CHAT)
+    verify(navHostController).navigate(eq(Route.CHAT), any<(NavOptionsBuilder) -> Unit>())
 
-        navigationActions.navigateTo(TopLevelDestinations.MAP)
-        verify(navHostController).navigate(eq(Route.MAP), any<(NavOptionsBuilder) -> Unit>())
+    navigationActions.navigateTo(TopLevelDestinations.MAP)
+    verify(navHostController).navigate(eq(Route.MAP), any<(NavOptionsBuilder) -> Unit>())
 
-        navigationActions.navigateTo(TopLevelDestinations.PROFIL)
-        verify(navHostController).navigate(eq(Route.PROFIL), any<(NavOptionsBuilder) -> Unit>())
+    navigationActions.navigateTo(TopLevelDestinations.PROFIL)
+    verify(navHostController).navigate(eq(Route.PROFIL), any<(NavOptionsBuilder) -> Unit>())
 
-        navigationActions.navigateTo(TopLevelDestinations.NEWBOOK)
-        verify(navHostController).navigate(eq(Route.NEWBOOK), any<(NavOptionsBuilder) -> Unit>())
-    }
+    navigationActions.navigateTo(TopLevelDestinations.NEWBOOK)
+    verify(navHostController).navigate(eq(Route.NEWBOOK), any<(NavOptionsBuilder) -> Unit>())
+  }
 
-    @Test
-    fun navigateToScreen(){
-        navigationActions.navigateTo(Screen.MAP)
-        verify(navHostController).navigate(Screen.MAP)
+  @Test
+  fun navigateToScreen() {
+    navigationActions.navigateTo(Screen.MAP)
+    verify(navHostController).navigate(Screen.MAP)
 
-        navigationActions.navigateTo(Screen.CHAT)
-        verify(navHostController).navigate(Screen.CHAT)
+    navigationActions.navigateTo(Screen.CHAT)
+    verify(navHostController).navigate(Screen.CHAT)
 
-        navigationActions.navigateTo(Screen.NEWBOOK)
-        verify(navHostController).navigate(Screen.NEWBOOK)
+    navigationActions.navigateTo(Screen.NEWBOOK)
+    verify(navHostController).navigate(Screen.NEWBOOK)
 
-        navigationActions.navigateTo(Screen.ADD_BOOK_ISBN)
-        verify(navHostController).navigate(Screen.ADD_BOOK_ISBN)
+    navigationActions.navigateTo(Screen.ADD_BOOK_ISBN)
+    verify(navHostController).navigate(Screen.ADD_BOOK_ISBN)
 
-        navigationActions.navigateTo(Screen.ADD_BOOK_SCAN)
-        verify(navHostController).navigate(Screen.ADD_BOOK_SCAN)
-    }
+    navigationActions.navigateTo(Screen.ADD_BOOK_SCAN)
+    verify(navHostController).navigate(Screen.ADD_BOOK_SCAN)
+  }
 
-    @Test
-    fun currentRouteAreCorrect(){
-        `when`(navHostController.currentDestination).thenReturn(navigationDestination)
-        `when`(navigationDestination.route).thenReturn(Route.NEWBOOK)
+  @Test
+  fun currentRouteAreCorrect() {
+    `when`(navHostController.currentDestination).thenReturn(navigationDestination)
+    `when`(navigationDestination.route).thenReturn(Route.NEWBOOK)
 
-        navigationActions.navigateTo(TopLevelDestinations.NEWBOOK)
-        assertThat(navigationActions.currentRoute(), `is`(Route.NEWBOOK))
+    navigationActions.navigateTo(TopLevelDestinations.NEWBOOK)
+    assertThat(navigationActions.currentRoute(), `is`(Route.NEWBOOK))
+  }
 
-    }
+  @Test
+  fun goBackCallPopBackStack() {
+    navigationActions.goBack()
+    verify(navHostController).popBackStack()
+  }
 
-    @Test
-    fun goBackCallPopBackStack(){
-        navigationActions.goBack()
-        verify(navHostController).popBackStack()
-    }
-
-    @Test
-    fun topLevelDestinationsHaveCorrectIcon(){
-        assertThat(TopLevelDestinations.CHAT.icon, `is`(Icons.Filled.MailOutline))
-        assertThat(TopLevelDestinations.MAP.icon, `is`(Icons.Outlined.Place))
-        assertThat(TopLevelDestinations.NEWBOOK.icon, `is`(Icons.Outlined.AddCircle))
-        assertThat(TopLevelDestinations.PROFIL.icon, `is`(Icons.Outlined.AccountCircle))
-    }
-
+  @Test
+  fun topLevelDestinationsHaveCorrectIcon() {
+    assertThat(TopLevelDestinations.CHAT.icon, `is`(Icons.Filled.MailOutline))
+    assertThat(TopLevelDestinations.MAP.icon, `is`(Icons.Outlined.Place))
+    assertThat(TopLevelDestinations.NEWBOOK.icon, `is`(Icons.Outlined.AddCircle))
+    assertThat(TopLevelDestinations.PROFIL.icon, `is`(Icons.Outlined.AccountCircle))
+  }
 }
