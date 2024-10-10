205a72ebf1ba0dc501177e39e35bea066bb5b144
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-10-10 19:47:43 +0200

    Fix user unit test

--- app/src/test/java/com/android/bookswap/data/UserTest.kt
+++ app/src/test/java/com/android/bookswap/data/UserTest.kt
@@ -10,7 +10,14 @@ class UserTest {
   @Test
   fun checkCreate() {
     val u1 = User()
-    assertEquals(u1, User("", "", "", "", "", Address(Locale.getDefault()), "", ""))
+    assertEquals(u1.greeting, "")
+    assertEquals(u1.firstName, "")
+    assertEquals(u1.lastName, "")
+    assertEquals(u1.email, "")
+    assertEquals(u1.phoneNumber, "")
+    assertEquals(u1.profilePictureUrl, "")
+    assertEquals(u1.userId, "")
+    assertEquals(u1.address, Address(Locale.getDefault()))
   }
 
   @Test
@@ -31,39 +38,23 @@ class UserTest {
             address,
             "dummyPic.png",
             "dummyUUID0000")
-    assertEquals(
-        u1,
-        User(
-            "M.",
-            "John",
-            "Doe",
-            "John.Doe@example.com",
-            "+41223456789",
-            address,
-            "dummyPic.png",
-            "dummyUUID0000"))
+    assertEquals(u1.greeting, "M.")
+    assertEquals(u1.firstName, "John")
+    assertEquals(u1.lastName, "Doe")
+    assertEquals(u1.email, "Hohn.Doe@example.com")
+    assertEquals(u1.phoneNumber, "+41223456789")
+    assertEquals(u1.profilePictureUrl, "dummyPic.png")
+    assertEquals(u1.userId, "dummyUUID0000")
+    assertEquals(u1.address, address)
     u1.greeting = "Mr."
-    assertNotEquals(
-        u1,
-        User(
-            "M.",
-            "John",
-            "Doe",
-            "John.Doe@example.com",
-            "+41223456789",
-            address,
-            "dummyPic.png",
-            "dummyUUID0000"))
-    assertEquals(
-        u1,
-        User(
-            "Mr.",
-            "John",
-            "Doe",
-            "John.Doe@example.com",
-            "+41223456789",
-            address,
-            "dummyPic.png",
-            "dummyUUID0000"))
+    assertNotEquals(u1.greeting, "M.")
+    assertEquals(u1.greeting, "Mr.")
+    assertEquals(u1.firstName, "John")
+    assertEquals(u1.lastName, "Doe")
+    assertEquals(u1.email, "Hohn.Doe@example.com")
+    assertEquals(u1.phoneNumber, "+41223456789")
+    assertEquals(u1.profilePictureUrl, "dummyPic.png")
+    assertEquals(u1.userId, "dummyUUID0000")
+    assertEquals(u1.address, address)
   }
 }
