a3baf38a61ed288e9a36920cfdb7229ae12c443d
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-10-15 09:26:50 +0200

    fix keys in sonar properties
    change sonar project key, project name and organization key to the correct ones from sonar's project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,9 +94,9 @@ android {
 
 sonar {
     properties {
-        property("sonar.projectKey", "gf_android-bookswap")
-        property("sonar.projectName", "Android-BookSwap")
-        property("sonar.organization", "gabrielfleischer")
+        property("sonar.projectKey", "BookswapEPFL_Bookswap")
+        property("sonar.projectName", "BookSwapApp")
+        property("sonar.organization", "bookswapepfl")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
         property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
