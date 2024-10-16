99712faedb52fdd1a0e46971b0a5b24fe6951e8b
EdenKahane <kahaneeden@protonmail.com>
2024-10-17 01:24:42 +0200

    feature: add modifier parameter to components

--- app/src/main/java/com/android/bookswap/ui/components/ButtonComponent.kt
+++ app/src/main/java/com/android/bookswap/ui/components/ButtonComponent.kt
@@ -1,6 +1,7 @@
 package com.android.bookswap.ui.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,22 +13,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.android.bookswap.ui.theme.Primary
+import com.android.bookswap.ui.theme.ColorVariable
 
 @Composable
 fun ButtonComponent(
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     content: @Composable() (RowScope.() -> Unit)
 ) {
   OutlinedButton(
       modifier =
-          Modifier.border(
-                  width = 1.dp, color = Color.Black, shape = RoundedCornerShape(size = 28.dp))
+          modifier
+              .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(size = 28.dp))
               .padding(0.5.dp)
               .width(120.dp)
-              .height(38.dp),
-      colors = ButtonDefaults.buttonColors(containerColor = Primary),
+              .height(38.dp)
+              .focusable(),
+      colors = ButtonDefaults.buttonColors(containerColor = ColorVariable.Primary),
       onClick = onClick,
       enabled = enabled,
       content = content)
--- app/src/main/java/com/android/bookswap/ui/components/FieldComponent.kt
+++ app/src/main/java/com/android/bookswap/ui/components/FieldComponent.kt
@@ -1,31 +1,34 @@
 package com.android.bookswap.ui.components
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.android.bookswap.ui.theme.AccentSecondary
-import com.android.bookswap.ui.theme.Secondary
+import com.android.bookswap.ui.theme.ColorVariable
 
 @Composable
-fun FieldComponent(labelText: String, value: String, onValueChange: (String) -> Unit = {}) {
+fun FieldComponent(
+    labelText: String,
+    value: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit = {}
+) {
   OutlinedTextField(
-      modifier = Modifier.width(290.dp).height(42.dp),
+      modifier = modifier.scale(1.0f, 0.8f),
       shape = RoundedCornerShape(25.dp),
       value = value,
       colors =
           TextFieldDefaults.colors(
-              unfocusedContainerColor = Secondary,
-              focusedContainerColor = Secondary,
-              cursorColor = Secondary, // Custom green for the cursor
-              focusedLabelColor = Secondary, // Custom green for focused label
-              unfocusedLabelColor = Secondary, // Lighter color for unfocused label
+              unfocusedContainerColor = ColorVariable.Secondary,
+              focusedContainerColor = ColorVariable.Secondary,
+              cursorColor = ColorVariable.Secondary, // Custom green for the cursor
+              focusedLabelColor = ColorVariable.Secondary, // Custom green for focused label
+              unfocusedLabelColor = ColorVariable.Secondary, // Lighter color for unfocused label
           ),
       onValueChange = onValueChange,
       label = {
@@ -33,7 +36,7 @@ fun FieldComponent(labelText: String, value: String, onValueChange: (String) -> 
             labelText,
             style =
                 TextStyle(
-                    color = AccentSecondary,
+                    color = ColorVariable.AccentSecondary,
                 ),
         )
       })
