244265cfce265a8bc46ff54f408b52ee2ec0f8db
EdenKahane <kahaneeden@protonmail.com>
2024-10-17 21:41:39 +0200

    fix: AddISBNScreen now use BackButtonComponent

--- app/src/main/java/com/android/bookswap/ui/books/add/AddISBNScreen.kt
+++ app/src/main/java/com/android/bookswap/ui/books/add/AddISBNScreen.kt
@@ -10,14 +10,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.bookswap.data.repository.BooksRepository
 import com.android.bookswap.data.source.api.GoogleBookDataSource
+import com.android.bookswap.ui.components.BackButtonComponent
 import com.android.bookswap.ui.components.ButtonComponent
 import com.android.bookswap.ui.components.FieldComponent
 import com.android.bookswap.ui.navigation.NavigationActions
@@ -62,17 +60,7 @@ fun AddISBNScreen(navigationActions: NavigationActions, booksRepository: BooksRe
                 Row(
                     horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth(0.85f)) {
-                      IconButton(
-                          onClick = { /*TODO navigate back*/},
-                          modifier = Modifier.testTag("go_back_button"),
-                      ) {
-                        Icon(
-                            Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back Icon",
-                            tint = ColorVariable.Accent,
-                            modifier = Modifier.size(64.dp),
-                        )
-                      }
+                      BackButtonComponent(navigationActions)
                       Text(
                           text = "Search ISBN",
                           style =
