d850e150a4dd0901f3d262e0bf26634cbd28e76a
LeniCadorinEpfl <leni.cadorin@epfl.ch>
2024-10-17 02:44:32 +0200

    fix: remove hard-coded value and add comments

--- app/src/main/java/com/android/bookswap/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/bookswap/ui/navigation/BottomNavigationMenu.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -15,24 +13,34 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.android.bookswap.ui.theme.ColorVariable
 
+/** Constants * */
+private val BOTTOM_NAV_HEIGHT = 60.dp
+private val ROUNDED_CORNER_SIZE = 60.dp
+
+/**
+ * This Composable function creates a bottom navigation menu for the app Displaying different tabs
+ * (destinations) based on the provided list of destinations (tabList).
+ */
 @Composable
 fun BottomNavigationMenu(
     onTabSelect: (TopLevelDestination) -> Unit,
     tabList: List<TopLevelDestination>,
     selectedItem: String
 ) {
-    BottomNavigation(
-        modifier = Modifier.fillMaxWidth().height(60.dp).testTag("bottomNavigationMenu"),
-        backgroundColor = ColorVariable.Primary, //Color of the bottom navigation bar
-        content = {
-            tabList.forEach { tab ->
-                BottomNavigationItem(
-                    icon = { Icon(tab.icon, contentDescription = null) },
-                    //label = { Text(tab.textId) },
-                    selected = tab.route == selectedItem,
-                    onClick = { onTabSelect(tab) },
-                    modifier = Modifier.clip(RoundedCornerShape(60.dp)).testTag(tab.textId))
-            }
-        },
-    )
+  BottomNavigation(
+      modifier = Modifier.fillMaxWidth().height(BOTTOM_NAV_HEIGHT).testTag("bottomNavigationMenu"),
+      backgroundColor = ColorVariable.Primary, // Color of the bottom navigation bar
+      content = {
+        tabList.forEach { tab ->
+          BottomNavigationItem(
+              icon = {
+                // Display the icon for each tab
+                Icon(tab.icon, contentDescription = null, tint = ColorVariable.BackGround)
+              },
+              selected = tab.route == selectedItem,
+              onClick = { onTabSelect(tab) },
+              modifier = Modifier.clip(RoundedCornerShape(ROUNDED_CORNER_SIZE)).testTag(tab.textId))
+        }
+      },
+  )
 }
