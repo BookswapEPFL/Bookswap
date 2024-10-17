a38bf99ed8f195d1ecdfbfc4fcc0ad391af1e9b6
TheoSchlaeppi <81753102+TheoSchlaeppi@users.noreply.github.com>
2024-10-17 22:07:29 +0200

    test: add a test to check the selected filter

--- app/src/androidTest/java/com/android/bookswap/ui/map/FilterMapScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/map/FilterMapScreenTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
 import com.android.bookswap.model.map.BookFilter
 import com.android.bookswap.ui.navigation.NavigationActions
@@ -35,5 +36,32 @@ class FilterMapScreenTest {
     composeTestRule.onNodeWithTag("filter_buttonFilter_test").assertIsDisplayed()
     composeTestRule.onNodeWithTag("filter_buttonFilter_test").assertHasClickAction()
     composeTestRule.onNodeWithTag("filter_buttonFilter_test").assertTextEquals("test")
+  }
+
+  @Test
+  fun testButtonBlockWithSelected() {
+    val selected = mutableListOf("test1")
+
+    composeTestRule.setContent {
+      ButtonBlock(listOf("test1", "test2", "test3", "test4"), selected) { newSelection ->
+        selected.clear()
+        selected.addAll(newSelection)
+      }
+    }
+
+    composeTestRule
+        .onNodeWithTag("filter_buttonFilter_test1")
+        .assertIsDisplayed()
+        .assertHasClickAction()
+        .performClick()
+
+    assert(!selected.contains("test1"))
+
+    composeTestRule
+        .onNodeWithTag("filter_buttonFilter_test2")
+        .assertIsDisplayed()
+        .assertHasClickAction()
+        .performClick()
+    assert(selected.contains("test2"))
   }
 }
