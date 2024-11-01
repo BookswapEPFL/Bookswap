6a05f7a0faafcba88905125b1814217dc196de7f
matiisalv <matias.salvade@epfl.ch>
2024-11-01 10:11:18 +0100

    fix: fix kfmtv

--- app/src/main/java/com/android/bookswap/MainActivity.kt
+++ app/src/main/java/com/android/bookswap/MainActivity.kt
@@ -129,45 +129,44 @@ class MainActivity : ComponentActivity() {
   }
 }
 
-  // Temporary user list for the map as it is not yet linked to the database.
-  // Better to see how the map screen should look like at the end.
-  // Need to be removed in the future.
-  val user =
-      listOf(
-          TempUser(
-              latitude = 0.0,
-              longitude = 0.0,
-              listBook =
-                  listOf(
-                      DataBook(
-                          uuid = UUID.randomUUID(),
-                          title = "Book 1",
-                          author = "Author 1",
-                          description = "Description of Book 1",
-                          rating = 5,
-                          photo = null,
-                          language = BookLanguages.ENGLISH,
-                          isbn = null,
-                          genres = listOf(BookGenres.FANTASY)),
-                      DataBook(
-                          uuid = UUID.randomUUID(),
-                          title = "Book 2",
-                          author = "Author 2",
-                          description = "Description of Book 2",
-                          rating = 4,
-                          photo = null,
-                          language = BookLanguages.FRENCH,
-                          isbn = null,
-                          genres = listOf(BookGenres.FICTION)),
-                      DataBook(
-                          uuid = UUID.randomUUID(),
-                          title = "Book 3",
-                          author = "Author 3",
-                          description = "Description of Book 3",
-                          rating = null,
-                          photo = null,
-                          language = BookLanguages.GERMAN,
-                          isbn = null,
-                          genres = listOf(BookGenres.SCIENCEFICTION, BookGenres.AUTOBIOGRAPHY)),
-                  )))
-
+// Temporary user list for the map as it is not yet linked to the database.
+// Better to see how the map screen should look like at the end.
+// Need to be removed in the future.
+val user =
+    listOf(
+        TempUser(
+            latitude = 0.0,
+            longitude = 0.0,
+            listBook =
+                listOf(
+                    DataBook(
+                        uuid = UUID.randomUUID(),
+                        title = "Book 1",
+                        author = "Author 1",
+                        description = "Description of Book 1",
+                        rating = 5,
+                        photo = null,
+                        language = BookLanguages.ENGLISH,
+                        isbn = null,
+                        genres = listOf(BookGenres.FANTASY)),
+                    DataBook(
+                        uuid = UUID.randomUUID(),
+                        title = "Book 2",
+                        author = "Author 2",
+                        description = "Description of Book 2",
+                        rating = 4,
+                        photo = null,
+                        language = BookLanguages.FRENCH,
+                        isbn = null,
+                        genres = listOf(BookGenres.FICTION)),
+                    DataBook(
+                        uuid = UUID.randomUUID(),
+                        title = "Book 3",
+                        author = "Author 3",
+                        description = "Description of Book 3",
+                        rating = null,
+                        photo = null,
+                        language = BookLanguages.GERMAN,
+                        isbn = null,
+                        genres = listOf(BookGenres.SCIENCEFICTION, BookGenres.AUTOBIOGRAPHY)),
+                )))
