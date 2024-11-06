e6bc96ffe887deae5b7043a1723e18e146570aa0
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-11-06 15:18:25 +0100

    Update ButtonComponent to extend support of Material3 theming using tokens
    Extend ButtonComponent to fully support and customize theming using Material3 design tokens.
    The ButtonComponent implementation is based on the OutlinedButton from Material3

--- app/src/main/java/com/android/bookswap/ui/components/ButtonComponent.kt
+++ app/src/main/java/com/android/bookswap/ui/components/ButtonComponent.kt
@@ -1,37 +1,191 @@
 package com.android.bookswap.ui.components
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.focusable
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.android.bookswap.ui.theme.ColorVariable
+import com.android.bookswap.ui.theme.BookSwapAppTheme
 
 @Composable
 fun ButtonComponent(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    border: BorderStroke? = ButtonDefaults.outlinedButtonBorder,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     content: @Composable() (RowScope.() -> Unit)
 ) {
-  OutlinedButton(
-      modifier =
-          modifier
-              .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(size = 28.dp))
-              .padding(0.5.dp)
-              .width(120.dp)
-              .height(38.dp)
-              .focusable(),
-      colors = ButtonDefaults.buttonColors(containerColor = ColorVariable.Primary),
+  val colors = ButtonDefaults.outlinedButtonColors()
+  val contentPadding = PaddingValues(8.dp, 8.dp)
+  val textPadding =
+      PaddingValues(
+          12.dp,
+          contentPadding.calculateTopPadding(),
+          12.dp,
+          contentPadding.calculateBottomPadding())
+  val shape = CircleShape
+  val containerColor =
+      if (enabled) {
+        colors.containerColor
+      } else {
+        colors.disabledContainerColor
+      }
+  val contentColor =
+      if (enabled) {
+        colors.contentColor
+      } else {
+        colors.disabledContentColor
+      }
+
+  Surface(
       onClick = onClick,
+      modifier =
+          modifier.minimumInteractiveComponentSize().clip(shape).semantics { role = Role.Button },
       enabled = enabled,
-      content = content)
+      shape = shape,
+      color = containerColor,
+      contentColor = contentColor,
+      border = border,
+      interactionSource = interactionSource) {
+        val mergedStyle = LocalTextStyle.current.merge(MaterialTheme.typography.labelLarge)
+        CompositionLocalProvider(
+            LocalContentColor provides contentColor, LocalTextStyle provides mergedStyle) {
+              Row(
+                  Modifier.defaultMinSize(
+                          minWidth = ButtonDefaults.MinHeight, minHeight = ButtonDefaults.MinHeight)
+                      .padding(contentPadding),
+                  horizontalArrangement = Arrangement.Center,
+                  verticalAlignment = Alignment.CenterVertically,
+                  content = content)
+            }
+      }
+}
+
+@Composable
+fun IconButtonComponent(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    border: BorderStroke? = null,
+    tint: Color = LocalContentColor.current,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable() (() -> Unit)
+) {
+  val colors = IconButtonDefaults.iconButtonColors() // ButtonDefaults.outlinedButtonColors()
+
+  val shape = CircleShape
+  val containerColor =
+      if (enabled) {
+        colors.containerColor
+      } else {
+        colors.disabledContainerColor
+      }
+  val contentColor =
+      if (enabled) {
+        tint
+      } else {
+        tint.copy(colors.disabledContentColor.alpha)
+      }
+
+  Surface(
+      onClick = onClick,
+      modifier =
+          modifier.minimumInteractiveComponentSize().clip(shape).semantics { role = Role.Button },
+      enabled = enabled,
+      shape = shape,
+      color = containerColor,
+      contentColor = contentColor,
+      border = border,
+      interactionSource = interactionSource) {
+        val mergedStyle = LocalTextStyle.current.merge(MaterialTheme.typography.labelLarge)
+        CompositionLocalProvider(
+            LocalContentColor provides contentColor, LocalTextStyle provides mergedStyle) {
+              Row(
+                  Modifier.defaultMinSize(
+                      minWidth = ButtonDefaults.MinHeight, minHeight = ButtonDefaults.MinHeight),
+                  horizontalArrangement = Arrangement.Center,
+                  verticalAlignment = Alignment.CenterVertically,
+                  content = { content() })
+            }
+      }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, name = "LightMode")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "DarkMode")
+@Composable
+fun ButtonComponentPreview() {
+  val text = "Button"
+  BookSwapAppTheme {
+    Column(verticalArrangement = Arrangement.spacedBy(1.dp)) {
+      ButtonComponent({}) { Text(text) }
+      TextButton(
+          onClick = { /*TODO*/},
+          modifier = Modifier.padding(0.5.dp),
+          border = ButtonDefaults.outlinedButtonBorder) {
+            Text(text)
+          }
+      ButtonComponent(onClick = { /*TODO*/}, border = null) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            // tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = "Back",
+            modifier = Modifier.testTag("backIcon").size(32.dp))
+      }
+      IconButtonComponent(onClick = { /*TODO*/}, tint = MaterialTheme.colorScheme.outline) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            // tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = "Back",
+            modifier = Modifier.testTag("backIcon").size(32.dp))
+      }
+      IconButton(onClick = { /*TODO*/}) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            // tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = "Back",
+            modifier = Modifier.testTag("backIcon").size(32.dp))
+      }
+      TextButton(onClick = { /*TODO*/}) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            // tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = "Back",
+            modifier = Modifier.testTag("backIcon").size(32.dp))
+      }
+    }
+  }
 }
