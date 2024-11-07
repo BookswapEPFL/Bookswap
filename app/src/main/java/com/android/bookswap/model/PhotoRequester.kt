b7719b7a79f62a7b5fd5005a98fa1cb8e3d119da
EdenKahane <kahaneeden@protonmail.com>
2024-11-07 23:16:48 +0100

    fix: missing code documentation

--- app/src/main/java/com/android/bookswap/model/PhotoRequester.kt
+++ app/src/main/java/com/android/bookswap/model/PhotoRequester.kt
@@ -14,21 +14,29 @@ import androidx.core.content.FileProvider
 import com.android.bookswap.BuildConfig
 import java.io.File
 
+/**
+ * Easy code abstraction allowing the user to open their photo app and take a photo of their choice
+ * and then confirm it. Please call [Init] (Composable) in your UI (doesn't matter where)
+ *
+ * @param context the ui context
+ * @param callback what to do when the user has finished (or failed) taking a photo
+ */
 class PhotoRequester(
     private val context: Context,
     private val callback: (Result<ImageBitmap>) -> Unit
 ) {
-  private var initialized = false
+  private var initialized = false // Confirm that init was called
 
   private lateinit var permissionLauncher: ManagedActivityResultLauncher<String, Boolean>
   private lateinit var cameraLauncher: ManagedActivityResultLauncher<Uri, Boolean>
 
-  private var fileUri: Uri? = null
+  private var fileUri: Uri? = null // Temp file path
 
   @Composable
   fun Init() {
     initialized = true
 
+    // When a photo is called
     cameraLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { isImageSaved ->
           if (isImageSaved) {
@@ -40,7 +48,7 @@ class PhotoRequester(
             callback(Result.failure(Exception("Image could not be saved in phone.")))
           }
         }
-
+    // Ask for permission before using the camera.
     permissionLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {
             permissionGranted ->
@@ -53,12 +61,14 @@ class PhotoRequester(
         }
   }
 
+  /** Request a photo from the user, this will ends-up calling [callback] */
   fun requestPhoto() {
     if (!initialized) throw Error("Always call Init() before using requestPhoto()")
 
     permissionLauncher.launch(Manifest.permission.CAMERA)
   }
 
+  /** Generate a Uri for temporary file storage in the app cache. */
   private fun getTempUri(): Uri {
     val tempFile =
         File.createTempFile(
