ce447fedd50189cb423d019b481bef8377334dc3
EdenKahane <kahaneeden@protonmail.com>
2024-11-08 01:53:12 +0100

    fix: removed and fixed bloated tests

--- app/src/androidTest/java/com/android/bookswap/ui/profile/UserProfileScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/profile/UserProfileScreenTest.kt
@@ -1,14 +1,15 @@
 package com.android.bookswap.ui.profile
 
-import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.bookswap.data.DataUser
-import com.android.bookswap.data.repository.UsersRepository
 import com.android.bookswap.model.UserViewModel
 import com.android.bookswap.screen.UserProfileScreen
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.every
+import io.mockk.mockk
+import java.util.UUID
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,22 +24,23 @@ import org.junit.runner.RunWith
 class UserProfileScreenTest : TestCase() {
 
   @get:Rule val composeTestRule = createComposeRule()
+  private val standardUser =
+      DataUser(
+          UUID.randomUUID(),
+          "M.",
+          "John",
+          "Doe",
+          "John.Doe@example.com",
+          "+41223456789",
+          0.0,
+          0.0,
+          "dummyPic.png")
 
   @Before
   fun setup() {
-    val userVM = UserViewModel("email@example.com", MockUserRepo())
-
-    userVM.updateUser(
-        DataUser(
-            "M.",
-            "John",
-            "Doe",
-            "John.Doe@example.com",
-            "+41223456789",
-            0.0,
-            0.0,
-            "dummyPic.png",
-            "dummyUUID0000"))
+    val userVM: UserViewModel = mockk()
+    every { userVM.getUser(any()) } returns standardUser
+    every { userVM.uuid } returns standardUser.userUUID
 
     composeTestRule.setContent { UserProfile(userVM = userVM) }
   }
@@ -77,7 +79,6 @@ class UserProfileScreenTest : TestCase() {
     }
   }
 
-  @OptIn(ExperimentalTestApi::class)
   @Test
   fun testEdit() {
     run(testName = "assertEditAction") {
@@ -119,69 +120,5 @@ class UserProfileScreenTest : TestCase() {
         }
       }
     }
-  }
-}
-
-class MockUserRepo : UsersRepository {
-  val mockUserList =
-      mutableMapOf(
-          "usr_01_jd" to
-              DataUser(
-                  "M.",
-                  "John",
-                  "Doe",
-                  "john.doe@example.com",
-                  "+41223456789",
-                  0.0,
-                  0.0,
-                  "",
-                  "usr_01_jd"),
-          "usr_02_jd" to
-              DataUser(
-                  "Mr.",
-                  "Jones",
-                  "Douse",
-                  "jon.doe@example.com",
-                  "+41234567890",
-                  0.0,
-                  0.0,
-                  "",
-                  "usr_02_jd"),
-          "usr_03_jd" to
-              DataUser(
-                  "Ms.",
-                  "Jo",
-                  "Doe",
-                  "jo.doe@example.com",
-                  "+41765432198",
-                  0.0,
-                  0.0,
-                  "",
-                  "usr_03_jd"),
-      )
-
-  override fun getUsers(callback: (Result<List<DataUser>>) -> Unit) {
-    callback(Result.success(mockUserList.values.toList()))
-  }
-
-  override fun getUser(uuid: String, callback: (Result<DataUser>) -> Unit) {
-    val usr = mockUserList.get(uuid)
-    if (usr != null) {
-      callback(Result.success(usr))
-    } else {
-      callback(Result.failure(Throwable("User not found")))
-    }
-  }
-
-  override fun addUser(dataUser: DataUser, callback: (Result<Unit>) -> Unit) {
-    mockUserList.put(dataUser.userUUID, dataUser)
-  }
-
-  override fun updateUser(dataUser: DataUser, callback: (Result<Unit>) -> Unit) {
-    mockUserList.put(dataUser.userUUID, dataUser)
-  }
-
-  override fun deleteUser(uuid: String, callback: (Result<Unit>) -> Unit) {
-    mockUserList.remove(uuid)
   }
 }
