f452cca56168b2706a85a74f8c3643e133a48b0e
LeniCadorinEpfl <leni.cadorin@epfl.ch>
2024-11-11 15:49:12 +0100

    fix: code formatting issues with ktfmt

--- app/src/test/java/com/android/bookswap/ui/navigation/NavigationCurrentLocation.kt
+++ app/src/test/java/com/android/bookswap/ui/navigation/NavigationCurrentLocation.kt
@@ -10,42 +10,47 @@ import org.junit.Test
 
 class NavigationCurrentLocation {
 
-    private lateinit var navController: NavHostController
-    private lateinit var navigationActions: NavigationActions
+  private lateinit var navController: NavHostController
+  private lateinit var navigationActions: NavigationActions
 
-    @Before
-    fun setup() {
-        navController = mockk(relaxed = true)
-        navigationActions = NavigationActions(navController)
+  @Before
+  fun setup() {
+    navController = mockk(relaxed = true)
+    navigationActions = NavigationActions(navController)
 
-        val currentBackStackEntry = mockk<NavBackStackEntry>()
-        every { navController.currentBackStackEntry } returns currentBackStackEntry
+    val currentBackStackEntry = mockk<NavBackStackEntry>()
+    every { navController.currentBackStackEntry } returns currentBackStackEntry
 
-        val destination = mockk<NavDestination>()
-        every { currentBackStackEntry.destination } returns destination
+    val destination = mockk<NavDestination>()
+    every { currentBackStackEntry.destination } returns destination
+  }
+
+  @Test
+  fun `navigateTo does not navigate when already on destination`() {
+    // Arrange: Set up the current route to match the target destination
+    every { navController.currentBackStackEntry?.destination?.route } returns
+        TopLevelDestinations.MAP.route
+
+    // Act: Try to navigate to the MAP screen
+    navigationActions.navigateTo(TopLevelDestinations.MAP)
+
+    // Assert: Verify that navigate is not called since we are already on MAP
+    verify(exactly = 0) { navController.navigate(any<String>()) }
+  }
+
+  @Test
+  fun `navigateTo navigates when not on destination`() {
+    // Arrange: Set up the current route to be different from the target destination
+    every { navController.currentBackStackEntry?.destination?.route } returns
+        TopLevelDestinations.CHAT.route
+
+    // Act: Try to navigate to the MAP screen
+    navigationActions.navigateTo(TopLevelDestinations.MAP)
+
+    // Assert: Verify that navigate is called since we are not on MAP
+    verify(exactly = 1) {
+      navController.navigate(
+          eq(TopLevelDestinations.MAP.route), any<NavOptionsBuilder.() -> Unit>())
     }
-
-    @Test
-    fun `navigateTo does not navigate when already on destination`() {
-        // Arrange: Set up the current route to match the target destination
-        every { navController.currentBackStackEntry?.destination?.route } returns TopLevelDestinations.MAP.route
-
-        // Act: Try to navigate to the MAP screen
-        navigationActions.navigateTo(TopLevelDestinations.MAP)
-
-        // Assert: Verify that navigate is not called since we are already on MAP
-        verify(exactly = 0) { navController.navigate(any<String>()) }
-    }
-
-    @Test
-    fun `navigateTo navigates when not on destination`() {
-        // Arrange: Set up the current route to be different from the target destination
-        every { navController.currentBackStackEntry?.destination?.route } returns TopLevelDestinations.CHAT.route
-
-        // Act: Try to navigate to the MAP screen
-        navigationActions.navigateTo(TopLevelDestinations.MAP)
-
-        // Assert: Verify that navigate is called since we are not on MAP
-        verify(exactly = 1) { navController.navigate(eq(TopLevelDestinations.MAP.route), any<NavOptionsBuilder.() -> Unit>()) }
-    }
+  }
 }
