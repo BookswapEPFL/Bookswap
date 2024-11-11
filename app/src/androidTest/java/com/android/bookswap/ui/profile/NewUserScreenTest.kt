42678696558ef537fc8c3f731e79fdace71b1737
TheoSchlaeppi <81753102+TheoSchlaeppi@users.noreply.github.com>
2024-11-11 18:29:47 +0100

    chore: KTFmt formatting

--- app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
@@ -6,13 +6,9 @@ import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.android.bookswap.ui.navigation.NavigationActions
 import com.android.bookswap.ui.navigation.Route
-import com.android.bookswap.ui.navigation.Screen
-import com.android.bookswap.ui.navigation.TopLevelDestinations
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Before
@@ -20,75 +16,78 @@ import org.junit.Rule
 import org.junit.Test
 
 class NewUserScreenTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
-    private lateinit var navigationActions: NavigationActions
+  @get:Rule val composeTestRule = createComposeRule()
+  private lateinit var navigationActions: NavigationActions
 
-    @Before
-    fun setUp() {
-        navigationActions = mockk(relaxed = true)
-    }
+  @Before
+  fun setUp() {
+    navigationActions = mockk(relaxed = true)
+  }
 
-    @Test
-    fun allComponentsAreDisplayedWithCorrectTexts() {
-        composeTestRule.setContent {
-            NewUserScreen(navigationActions)
-        }
-        composeTestRule.onNodeWithTag("welcomeTxt")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextEquals("Welcome")
+  @Test
+  fun allComponentsAreDisplayedWithCorrectTexts() {
+    composeTestRule.setContent { NewUserScreen(navigationActions) }
+    composeTestRule
+        .onNodeWithTag("welcomeTxt")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextEquals("Welcome")
 
-        composeTestRule.onNodeWithTag("personalInfoTxt")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextEquals("Please fill in your personal information to start BookSwapping")
+    composeTestRule
+        .onNodeWithTag("personalInfoTxt")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextEquals("Please fill in your personal information to start BookSwapping")
 
-        composeTestRule.onNodeWithTag("profilPics")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertHasClickAction()
+    composeTestRule
+        .onNodeWithTag("profilPics")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertHasClickAction()
 
-        composeTestRule.onNodeWithTag("greetingTF")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextContains("Greeting")
+    composeTestRule
+        .onNodeWithTag("greetingTF")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextContains("Greeting")
 
-        composeTestRule.onNodeWithTag("firstnameTF")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextContains("Firstname")
+    composeTestRule
+        .onNodeWithTag("firstnameTF")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextContains("Firstname")
 
-        composeTestRule.onNodeWithTag("lastnameTF")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextContains("Lastname")
+    composeTestRule
+        .onNodeWithTag("lastnameTF")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextContains("Lastname")
 
-        composeTestRule.onNodeWithTag("emailTF")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextContains("Email")
+    composeTestRule
+        .onNodeWithTag("emailTF")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextContains("Email")
 
-        composeTestRule.onNodeWithTag("phoneTF")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextContains("Phone")
+    composeTestRule
+        .onNodeWithTag("phoneTF")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextContains("Phone")
 
-        composeTestRule.onNodeWithTag("CreateButton")
-            .assertExists()
-            .assertIsDisplayed()
-            .assertTextEquals("Create")
-            .assertHasClickAction()
-    }
+    composeTestRule
+        .onNodeWithTag("CreateButton")
+        .assertExists()
+        .assertIsDisplayed()
+        .assertTextEquals("Create")
+        .assertHasClickAction()
+  }
 
-    @Test
-    fun clickOnCreateButtonNavigatesToMap() {
-        composeTestRule.setContent {
-            NewUserScreen(navigationActions)
-        }
+  @Test
+  fun clickOnCreateButtonNavigatesToMap() {
+    composeTestRule.setContent { NewUserScreen(navigationActions) }
 
-        composeTestRule.onNodeWithTag("CreateButton")
-            .performClick()
-        verify { navigationActions.navigateTo(Route.MAP) }
-    }
+    composeTestRule.onNodeWithTag("CreateButton").performClick()
+    verify { navigationActions.navigateTo(Route.MAP) }
+  }
 }
--- app/src/main/java/com/android/bookswap/ui/profile/NewUser.kt
+++ app/src/main/java/com/android/bookswap/ui/profile/NewUser.kt
@@ -1,11 +1,9 @@
 package com.android.bookswap.ui.profile
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -43,172 +41,123 @@ import com.android.bookswap.ui.theme.ColorVariable
 
 @Composable
 fun NewUserScreen(navigaionActions: NavigationActions) {
-    val _email = remember { mutableStateOf("") }
-    val _phone = remember { mutableStateOf("") }
-    val _greeting = remember { mutableStateOf("") }
-    val _firstName = remember { mutableStateOf("") }
-    val _lastName = remember { mutableStateOf("") }
+  val _email = remember { mutableStateOf("") }
+  val _phone = remember { mutableStateOf("") }
+  val _greeting = remember { mutableStateOf("") }
+  val _firstName = remember { mutableStateOf("") }
+  val _lastName = remember { mutableStateOf("") }
 
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
-        modifier =
-        Modifier
-            .fillMaxSize()
-            .background(color = ColorVariable.BackGround)
-            .testTag("chat_messageList")
-    ) {
+  LazyColumn(
+      contentPadding = PaddingValues(16.dp),
+      modifier =
+          Modifier.fillMaxSize()
+              .background(color = ColorVariable.BackGround)
+              .testTag("chat_messageList")) {
         item {
-            Text(
-                "Welcome", modifier = Modifier
-                    .testTag("welcomeTxt")
-                    .fillMaxWidth(),
-
-                style = TextStyle(
-                    color = ColorVariable.Accent,
-                    fontWeight = FontWeight(600),
-                    textAlign = TextAlign.Center,
-                    fontSize = 40.sp,
-                )
-            )
+          Text(
+              "Welcome",
+              modifier = Modifier.testTag("welcomeTxt").fillMaxWidth(),
+              style =
+                  TextStyle(
+                      color = ColorVariable.Accent,
+                      fontWeight = FontWeight(600),
+                      textAlign = TextAlign.Center,
+                      fontSize = 40.sp,
+                  ))
         }
         item {
-            Text(
-                "Please fill in your personal information to start BookSwapping",
-                modifier = Modifier
-                    .testTag("personalInfoTxt")
-                    .fillMaxWidth(),
-                style = TextStyle(
-                    color = ColorVariable.Accent,
-                    fontWeight = FontWeight(400),
-                    textAlign = TextAlign.Center,
-                    fontSize = 18.sp,
-                )
-            )
+          Text(
+              "Please fill in your personal information to start BookSwapping",
+              modifier = Modifier.testTag("personalInfoTxt").fillMaxWidth(),
+              style =
+                  TextStyle(
+                      color = ColorVariable.Accent,
+                      fontWeight = FontWeight(400),
+                      textAlign = TextAlign.Center,
+                      fontSize = 18.sp,
+                  ))
         }
         item {
-            Card(
-                Modifier
-                    .testTag("editProfileContainer")
-                    .background(ColorVariable.BackGround),
-                colors = androidx.compose.material3.CardDefaults.cardColors(
-
-                ).copy(containerColor = ColorVariable.BackGround)
-            ) {
+          Card(
+              Modifier.testTag("editProfileContainer").background(ColorVariable.BackGround),
+              colors =
+                  androidx.compose.material3.CardDefaults.cardColors()
+                      .copy(containerColor = ColorVariable.BackGround)) {
                 Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .background(ColorVariable.BackGround),
+                    Modifier.fillMaxWidth().padding(16.dp).background(ColorVariable.BackGround),
                     Arrangement.Center,
-                    Alignment.CenterHorizontally
-                ) {
-                    IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp).testTag("profilPics")) {
-                        Icon(
-                            imageVector = Icons.Default.AccountCircle,
-                            contentDescription = "profile picture",
-                            tint = ColorVariable.Accent,
-                            modifier = Modifier.size(80.dp)
-                        )
+                    Alignment.CenterHorizontally) {
+                      IconButton(
+                          onClick = { /*TODO*/},
+                          modifier = Modifier.size(80.dp).testTag("profilPics")) {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = "profile picture",
+                                tint = ColorVariable.Accent,
+                                modifier = Modifier.size(80.dp))
+                          }
+                      OutlinedTextField(
+                          _greeting.value,
+                          { _greeting.value = it },
+                          Modifier.testTag("greetingTF").fillMaxWidth().padding(8.dp, 4.dp),
+                          label = { Text("Greeting") },
+                          placeholder = { Text("Mr.", Modifier, Color.Gray) },
+                          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                          singleLine = true)
+
+                      OutlinedTextField(
+                          _firstName.value,
+                          { _firstName.value = it },
+                          Modifier.testTag("firstnameTF").fillMaxWidth().padding(8.dp, 4.dp),
+                          label = { Text("Firstname") },
+                          placeholder = { Text("John", Modifier, Color.Gray) },
+                          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                          singleLine = true)
+
+                      OutlinedTextField(
+                          _lastName.value,
+                          { _lastName.value = it },
+                          Modifier.testTag("lastnameTF").fillMaxWidth().padding(8.dp, 4.dp),
+                          label = { Text("Lastname") },
+                          placeholder = { Text("Doe", Modifier, Color.Gray) },
+                          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                          singleLine = true)
+
+                      OutlinedTextField(
+                          _email.value,
+                          { _email.value = it },
+                          Modifier.testTag("emailTF").fillMaxWidth().padding(8.dp, 4.dp),
+                          label = { Text("Email") },
+                          placeholder = { Text("John.Doe@example.com", Modifier, Color.Gray) },
+                          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                          singleLine = true)
+
+                      OutlinedTextField(
+                          _phone.value,
+                          { _phone.value = it },
+                          Modifier.testTag("phoneTF").fillMaxWidth().padding(8.dp, 4.dp),
+                          label = { Text("Phone") },
+                          placeholder = { Text("+4122345678", Modifier, Color.Gray) },
+                          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                          singleLine = true)
                     }
-                    OutlinedTextField(
-                        _greeting.value,
-                        {
-                            _greeting.value = it
-                        },
-                        Modifier
-                            .testTag("greetingTF")
-                            .fillMaxWidth()
-                            .padding(8.dp, 4.dp),
-                        label = { Text("Greeting") },
-                        placeholder = { Text("Mr.", Modifier, Color.Gray) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                        singleLine = true
-                    )
-
-                    OutlinedTextField(
-                        _firstName.value,
-                        {
-                            _firstName.value = it
-                        },
-                        Modifier
-                            .testTag("firstnameTF")
-                            .fillMaxWidth()
-                            .padding(8.dp, 4.dp),
-                        label = { Text("Firstname") },
-                        placeholder = { Text("John", Modifier, Color.Gray) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                        singleLine = true
-                    )
-
-                    OutlinedTextField(
-                        _lastName.value,
-                        {
-                            _lastName.value = it
-                        },
-                        Modifier
-                            .testTag("lastnameTF")
-                            .fillMaxWidth()
-                            .padding(8.dp, 4.dp),
-                        label = { Text("Lastname") },
-                        placeholder = { Text("Doe", Modifier, Color.Gray) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                        singleLine = true
-                    )
-
-                    OutlinedTextField(
-                        _email.value,
-                        {
-                            _email.value = it
-                        },
-                        Modifier
-                            .testTag("emailTF")
-                            .fillMaxWidth()
-                            .padding(8.dp, 4.dp),
-                        label = { Text("Email") },
-                        placeholder = { Text("John.Doe@example.com", Modifier, Color.Gray) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        singleLine = true
-                    )
-
-                    OutlinedTextField(
-                        _phone.value,
-                        {
-                            _phone.value = it
-                        },
-                        Modifier
-                            .testTag("phoneTF")
-                            .fillMaxWidth()
-                            .padding(8.dp, 4.dp),
-                        label = { Text("Phone") },
-                        placeholder = { Text("+4122345678", Modifier, Color.Gray) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                        singleLine = true
-                    )
-                }
-            }
-
+              }
         }
         item {
-            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                Button(
-                    onClick = {
-                        navigaionActions.navigateTo(Route.MAP)
-                    },
-                    colors = ButtonDefaults.buttonColors(ColorVariable.Primary),
-                    modifier =
-                    Modifier.width(200.dp)
-                        .height(50.dp)
-                        .testTag("CreateButton")) {
-                    Text(
-                        text = "Create",
-                        textAlign = TextAlign.Center,
-                        style =
-                        TextStyle(
-                            color = ColorVariable.BackGround,
-                        ))
+          Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+            Button(
+                onClick = { navigaionActions.navigateTo(Route.MAP) },
+                colors = ButtonDefaults.buttonColors(ColorVariable.Primary),
+                modifier = Modifier.width(200.dp).height(50.dp).testTag("CreateButton")) {
+                  Text(
+                      text = "Create",
+                      textAlign = TextAlign.Center,
+                      style =
+                          TextStyle(
+                              color = ColorVariable.BackGround,
+                          ))
                 }
-            }
+          }
         }
-
-    }
+      }
 }
