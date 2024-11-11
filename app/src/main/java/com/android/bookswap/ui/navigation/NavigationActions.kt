cea9d1145b71d3c7e055aa3cb1edf378de297fa7
LeniCadorinEpfl <leni.cadorin@epfl.ch>
2024-11-11 15:38:45 +0100

    fix: modify navigation to prevent temporary black screen on redundant screen reloads

--- app/src/main/java/com/android/bookswap/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/android/bookswap/ui/navigation/NavigationActions.kt
@@ -59,20 +59,23 @@ open class NavigationActions(
    */
   open fun navigateTo(destination: TopLevelDestination) {
 
-    navController.navigate(destination.route) {
-      // Pop up to the start destination of the graph to
-      // avoid building up a large stack of destinations
-      popUpTo(navController.graph.findStartDestination().id) {
-        saveState = true
-        inclusive = true
-      }
+    // Only navigate if the route is different from the current route
+    if (!isCurrentDestination(destination.route)) {
+      navController.navigate(destination.route) {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        popUpTo(navController.graph.findStartDestination().id) {
+          saveState = true
+          inclusive = true
+        }
 
-      // Avoid multiple copies of the same destination when reelecting same item
-      launchSingleTop = true
+        // Avoid multiple copies of the same destination when reelecting same item
+        launchSingleTop = true
 
-      // Restore state when reelecting a previously selected item
-      if (destination.route != Route.AUTH) {
-        restoreState = true
+        // Restore state when reelecting a previously selected item
+        if (destination.route != Route.AUTH) {
+          restoreState = true
+        }
       }
     }
   }
@@ -90,7 +93,10 @@ open class NavigationActions(
           Screen.CHAT -> "$screen/$user1/$user2"
           else -> screen
         }
-    navController.navigate(route)
+    // Only navigate if the route is different from the current route
+    if (!isCurrentDestination(route)) {
+      navController.navigate(route)
+    }
   }
 
   /**
@@ -99,7 +105,10 @@ open class NavigationActions(
    * @param screen The screen to navigate to
    */
   open fun navigateTo(screen: String) {
-    navController.navigate(screen)
+    // Only navigate if the route is different from the current route
+    if (!isCurrentDestination(screen)) {
+      navController.navigate(screen)
+    }
   }
 
   /** Navigate back to the previous screen. */
@@ -114,5 +123,11 @@ open class NavigationActions(
    */
   open fun currentRoute(): String {
     return navController.currentDestination?.route ?: ""
+  }
+
+  private fun isCurrentDestination(route: String): Boolean {
+    // Retrieve the current route and check if it starts with the same route name
+    val currentRoute = currentRoute()
+    return currentRoute.startsWith(route)
   }
 }
