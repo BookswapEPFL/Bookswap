1c4cec05a3e99c5f92c3c5b70ef3a95fc10100d0
chockencore <sphillot@netplus.ch>
2024-11-12 13:38:07 +0100

    test: added tests for BookManager

--- app/src/test/java/com/android/bookswap/model/map/BookManagerTest.kt
+++ app/src/test/java/com/android/bookswap/model/map/BookManagerTest.kt
@@ -5,53 +5,83 @@ import com.android.bookswap.data.BookLanguages
 import com.android.bookswap.data.DataBook
 import com.android.bookswap.data.DataUser
 import com.android.bookswap.data.source.network.BooksFirestoreRepository
+import com.android.bookswap.ui.map.UserBooksWithLocation
 import io.mockk.every
 import io.mockk.mockk
 import java.util.UUID
+import junit.framework.TestCase.assertEquals
+import kotlin.math.abs
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Test
 
 class BookManagerTest {
-  private val users =
-      listOf(
-          DataUser(bookList = listOf(UUID(1, 2), UUID(2, 1)), longitude = -5.0, latitude = 0.0),
-          DataUser(bookList = listOf(UUID(1, 1)), longitude = 1.0, latitude = 2.0))
 
-  private val books =
-      listOf(
-          DataBook(
-              uuid = UUID(1, 2),
-              title = "Book 1",
-              author = "Author 1",
-              description = "Description of Book 1",
-              rating = 5,
-              photo = "url_to_photo_1",
-              language = BookLanguages.ENGLISH,
-              isbn = "123-456-789",
-              genres = listOf(BookGenres.FICTION, BookGenres.HORROR)),
-          DataBook(
-              uuid = UUID(2, 1),
-              title = "Book 2",
-              author = "Author 2",
-              description = "Description of Book 2",
-              rating = 4,
-              photo = "url_to_photo_2",
-              language = BookLanguages.FRENCH,
-              isbn = "234-567-890",
-              genres = listOf(BookGenres.FICTION)),
-          DataBook(
-              uuid = UUID(1, 1),
-              title = "Book 3",
-              author = "Author 3",
-              description = "Description of Book 3",
-              rating = 4,
-              photo = "url_to_photo_3",
-              language = BookLanguages.GERMAN,
-              isbn = "234-567-890",
-              genres = listOf(BookGenres.NONFICTION, BookGenres.HORROR)))
+  private val user1 =
+      DataUser(bookList = listOf(UUID(1, 2), UUID(2, 1)), longitude = 50.0, latitude = 50.0)
+  private val user2 = DataUser(bookList = listOf(UUID(1, 1)), longitude = 0.0, latitude = 0.0)
+  private val users = listOf(user2, user1)
 
-  private val geolocation1 = listOf(3.5, 8.0)
-  private val geolocation2 = listOf(-10.0, -10.0)
+  private val book1 =
+      DataBook(
+          uuid = UUID(1, 2),
+          title = "Book 1",
+          author = "Author 1",
+          description = "Description of Book 1",
+          rating = 5,
+          photo = "url_to_photo_1",
+          language = BookLanguages.ENGLISH,
+          isbn = "123-456-789",
+          genres = listOf(BookGenres.FICTION, BookGenres.HORROR))
+
+  private val book2 =
+      DataBook(
+          uuid = UUID(2, 1),
+          title = "Book 2",
+          author = "Author 2",
+          description = "Description of Book 2",
+          rating = 4,
+          photo = "url_to_photo_2",
+          language = BookLanguages.GERMAN,
+          isbn = "234-567-890",
+          genres = listOf(BookGenres.FICTION))
+
+  private val book3 =
+      DataBook(
+          uuid = UUID(1, 1),
+          title = "Book 3",
+          author = "Author 3",
+          description = "Description of Book 3",
+          rating = 4,
+          photo = "url_to_photo_3",
+          language = BookLanguages.GERMAN,
+          isbn = "234-567-890",
+          genres = listOf(BookGenres.NONFICTION, BookGenres.HORROR))
+
+  private val books = listOf(book3, book1, book2)
+
+  private val userBooksWithLocation1 =
+      UserBooksWithLocation(user1.longitude, user1.latitude, listOf(book1, book2))
+
+  private val userBooksWithLocation2 =
+      UserBooksWithLocation(user2.longitude, user2.latitude, listOf(book3))
+
+  private val userBooksWithLocation = listOf(userBooksWithLocation2, userBooksWithLocation1)
+
+  private val filteredBooksWithLocation =
+      listOf(
+          userBooksWithLocation2,
+          UserBooksWithLocation(user1.longitude, user1.latitude, emptyList()))
+
+  private val geolocation1 = listOf(0.0, 0.0)
+  private val geolocation2 = listOf(100.0, 100.0)
+
+  private val sortingTest: (Double, Double, Double, Double) -> Double =
+      { startLatitude, _, endLatitude, _ ->
+        abs(endLatitude - startLatitude)
+      }
 
   private lateinit var mockBookRepository: BooksFirestoreRepository
   private lateinit var mockGeolocation1: IGeolocation
@@ -67,7 +97,6 @@ class BookManagerTest {
         {
           firstArg<(List<DataBook>) -> Unit>().invoke(books)
         }
-
     mockGeolocation1 = mockk()
     every { mockGeolocation1.longitude } answers { MutableStateFlow(geolocation1[0]) }
     every { mockGeolocation1.latitude } answers { MutableStateFlow(geolocation1[1]) }
@@ -80,20 +109,48 @@ class BookManagerTest {
     every { mockBookFilter.genresFilter } answers { MutableStateFlow(listOf(BookGenres.HORROR)) }
     every { mockBookFilter.languagesFilter } answers
         {
-          MutableStateFlow(listOf(BookLanguages.FRENCH))
+          MutableStateFlow(listOf(BookLanguages.GERMAN))
         }
+    every { mockBookFilter.filterBooks(any()) } answers { listOf(book3) }
 
     mockBookFilterEmpty = mockk()
     every { mockBookFilterEmpty.genresFilter } answers { MutableStateFlow(emptyList()) }
     every { mockBookFilterEmpty.languagesFilter } answers { MutableStateFlow(emptyList()) }
+    every { mockBookFilterEmpty.filterBooks(any()) } answers { firstArg() }
   }
 
-  /*
-  @OptIn(ExperimentalCoroutinesApi::class)
   @Test
-  fun listMarkerBookChangedWhenFilterApplied() = runTest{
-    bookManager = BookManager(mockGeolocation1, mockBookRepository, users, mockBookFilterEmpty)
-    advanceUntilIdle()
+  fun defaultCaseNoFilterOrSortingNecessary() = runTest {
+    bookManager =
+        BookManager(mockGeolocation1, mockBookRepository, users, mockBookFilterEmpty, true) {
+            _,
+            _,
+            _,
+            _ ->
+          0.0
+        }
+    bookManager.filteredBooks.first { it != emptyList<DataBook>() }
     assertEquals(books, bookManager.filteredBooks.value)
-  }*/
+    assertEquals(userBooksWithLocation, bookManager.filteredUsers.value)
+  }
+
+  @Test
+  fun returnFilteredListOfBooks() = runTest {
+    bookManager =
+        BookManager(mockGeolocation1, mockBookRepository, users, mockBookFilter, true, sortingTest)
+    bookManager.filteredBooks.first { it != emptyList<DataBook>() }
+    assertEquals(listOf(book3), bookManager.filteredBooks.value)
+    assertEquals(filteredBooksWithLocation, bookManager.filteredUsers.value)
+  }
+
+  @Test
+  fun sortTheUsers() = runTest {
+    bookManager =
+        BookManager(
+            mockGeolocation2, mockBookRepository, users, mockBookFilterEmpty, true, sortingTest)
+    bookManager.filteredBooks.first { it != emptyList<DataBook>() }
+    assertEquals(listOf(book1, book2, book3), bookManager.filteredBooks.value)
+    assertEquals(
+        listOf(userBooksWithLocation1, userBooksWithLocation2), bookManager.filteredUsers.value)
+  }
 }
