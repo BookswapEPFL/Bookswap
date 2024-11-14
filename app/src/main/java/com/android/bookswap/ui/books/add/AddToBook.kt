b0926ee463df4c929971bf5e0a703047273fae41
chockencore <sphillot@netplus.ch>
2024-11-14 13:08:38 +0100

    feat: basic clean up of UI

--- app/src/main/java/com/android/bookswap/ui/books/add/AddToBook.kt
+++ app/src/main/java/com/android/bookswap/ui/books/add/AddToBook.kt
@@ -6,13 +6,20 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -22,6 +29,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -30,11 +39,17 @@ import com.android.bookswap.data.BookGenres
 import com.android.bookswap.data.BookLanguages
 import com.android.bookswap.data.DataBook
 import com.android.bookswap.data.repository.BooksRepository
+import com.android.bookswap.data.source.api.GoogleBookDataSource
+import com.android.bookswap.ui.components.ButtonComponent
+import com.android.bookswap.ui.components.FieldComponent
+import com.android.bookswap.ui.navigation.TopLevelDestinations
 import com.android.bookswap.ui.theme.ColorVariable
 import com.android.bookswap.ui.theme.ColorVariable.BackGround
 import com.android.bookswap.ui.theme.ColorVariable.Primary
 import com.android.bookswap.ui.theme.ColorVariable.Secondary
 import java.util.UUID
+
+private const val HORIZONTAL_PADDING = 30
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,14 +73,137 @@ fun AddToBookScreen(
 
   // Scaffold to provide basic UI structure with a top app bar
   Scaffold(
-      modifier = Modifier.testTag("addBookScreen").background(BackGround),
+      modifier = Modifier.testTag("addBookScreen"),
       topBar = topAppBar,
       bottomBar = bottomAppBar,
       content = { paddingValues ->
-        // Column layout to stack input fields vertically with spacing
-        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-          // Title Input Field
-          OutlinedTextField(
+          // Column layout to stack input fields vertically with spacing
+          Column(modifier = Modifier.background(BackGround).fillMaxWidth().fillMaxHeight()
+              .padding(paddingValues),
+              verticalArrangement = Arrangement.SpaceBetween){
+              // Title Input Field
+              FieldComponent(
+                  modifier = Modifier.testTag("title_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Title*",
+                  value = title
+              ) { title = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("genre_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Genre*",
+                  value = title
+              ) { title = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("author_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Author*",
+                  value = author
+              ) { author = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("description_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Description*",
+                  value = description
+              ) { description = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("rating_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Rating*",
+                  value = rating
+              ) { rating = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("isbn_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp), labelText = "ISBN", value = isbn
+              ) { isbn = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("photo_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Photo",
+                  value = photo
+              ) { photo = it }
+              FieldComponent(
+                  modifier = Modifier.testTag("language_field").fillMaxWidth()
+                      .padding(horizontal = HORIZONTAL_PADDING.dp),
+                  labelText = "Language*",
+                  value = language
+              ) { language = it }
+              ExposedDropdownMenuBox(
+                  expanded = expanded,
+                  onExpandedChange = { expanded = !expanded },
+                  modifier = Modifier.background(ColorVariable.BackGround) // Set background color here
+              ) {
+                  OutlinedTextField(
+                      value = selectedGenre?.Genre ?: "Select Genre",
+                      onValueChange = {},
+                      label = { Text("Genre") },
+                      readOnly = true,
+                      trailingIcon = {
+                          ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                      },
+                      modifier = Modifier.menuAnchor())
+                  ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                      BookGenres.values().forEach { genre ->
+                          DropdownMenuItem(
+                              text = {
+                                  Text(
+                                      text = genre.Genre,
+                                      // color = ColorVariable.Secondary // Green text in dropdownmenu
+                                  )
+                              },
+                              onClick = {
+                                  selectedGenre = genre
+                                  expanded = false
+                              })
+                      }
+                  }
+              }
+              ButtonComponent(
+                  modifier = Modifier.testTag("save_button").align(Alignment.CenterHorizontally).fillMaxWidth(0.5f), onClick = {})
+              { Text("Save") }/*
+          Button(
+              colors =
+              ButtonDefaults.buttonColors(
+                  containerColor = Primary, // Light green
+                  contentColor = BackGround),
+              onClick = {
+                  // Check if title and ISBN are not blank (required fields)
+                  if (title.isNotBlank() && isbn.isNotBlank() && selectedGenre != null) {
+                      // You can handle book object creation here (e.g., save the book)
+                      val book =
+                          createDataBook(
+                              context,
+                              repository.getNewUUID(),
+                              title,
+                              author,
+                              description,
+                              rating,
+                              photo,
+                              language,
+                              isbn,
+                              listOf(selectedGenre!!))
+                      if (book == null) {
+                          Toast.makeText(context, "Invalid argument", Toast.LENGTH_SHORT).show()
+                      } else {
+                          repository.addBook(book, OnSucess = {}, onFailure = {})
+                      }
+                  } else {
+                      // Show a Toast message if title or ISBN is empty
+                      Toast.makeText(context, "Title and ISBN are required.", Toast.LENGTH_SHORT).show()
+                  }
+              },
+              // Enable the button only if title and ISBN are filled
+              enabled = title.isNotBlank() && isbn.isNotBlank(),
+              modifier = Modifier.testTag("bookSave").testTag("Save")) {
+              // Text displayed on the button
+              Text("Save", modifier = Modifier.testTag("bookSave").testTag("Save"))
+          }*/
+      }
+      }
+  )
+
+
+          /*OutlinedTextField(
               value = title,
               onValueChange = { title = it },
               label = { Text("Title") },
@@ -115,84 +253,6 @@ fun AddToBookScreen(
                   }
                 }
               }
-
-          // Author Input Field
-          OutlinedTextField(
-              value = author,
-              onValueChange = { author = it },
-              label = { Text("Author") },
-              placeholder = { Text("Enter the author's name") },
-              modifier =
-                  Modifier.padding(paddingValues).testTag("inputBookAuthor").testTag("Author"),
-              colors =
-                  OutlinedTextFieldDefaults.colors(
-                      focusedBorderColor = Secondary, // Custom green for focused border
-                      unfocusedBorderColor = Secondary, // Lighter green for unfocused border
-                      cursorColor = Secondary, // Custom green for the cursor
-                      focusedLabelColor = Secondary, // Custom green for focused label
-                      unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ) // Adding padding to the input field
-              )
-          // Description Input Field
-          OutlinedTextField(
-              value = description,
-              onValueChange = { description = it },
-              label = { Text("Description") },
-              placeholder = { Text("Provide a description of the book") },
-              modifier = Modifier.testTag("inputBookDescription").testTag("Description"),
-              colors =
-                  OutlinedTextFieldDefaults.colors(
-                      focusedBorderColor = Secondary, // Custom green for focused border
-                      unfocusedBorderColor = Secondary, // Lighter green for unfocused border
-                      cursorColor = Secondary, // Custom green for the cursor
-                      focusedLabelColor = Secondary, // Custom green for focused label
-                      unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ))
-          // Rating Input Field
-          OutlinedTextField(
-              value = rating,
-              onValueChange = { rating = it },
-              label = { Text("Rating") },
-              placeholder = { Text("Rate the book (e.g. 4.5)") },
-              modifier = Modifier.testTag("inputBookRating"),
-              colors =
-                  OutlinedTextFieldDefaults.colors(
-                      focusedBorderColor = Secondary, // Custom green for focused border
-                      unfocusedBorderColor = Secondary, // Lighter green for unfocused border
-                      cursorColor = Secondary, // Custom green for the cursor
-                      focusedLabelColor = Secondary, // Custom green for focused label
-                      unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ))
-          // ISBN Input Field
-          OutlinedTextField(
-              value = isbn,
-              onValueChange = { isbn = it },
-              label = { Text("ISBN") },
-              placeholder = { Text("Enter the ISBN") },
-              modifier = Modifier.testTag("inputBookISBN").testTag("ISBN"),
-              colors =
-                  OutlinedTextFieldDefaults.colors(
-                      focusedBorderColor = Secondary, // Custom green for focused border
-                      unfocusedBorderColor = Secondary, // Lighter green for unfocused border
-                      cursorColor = Secondary, // Custom green for the cursor
-                      focusedLabelColor = Secondary, // Custom green for focused label
-                      unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ))
-          // Photo  Input Field
-          OutlinedTextField(
-              value = photo,
-              onValueChange = { photo = it },
-              label = { Text("Photo ") },
-              placeholder = { Text("Enter a photo of the books") },
-              modifier = Modifier.testTag("inputBookPhoto").testTag("Photo"),
-              colors =
-                  OutlinedTextFieldDefaults.colors(
-                      focusedBorderColor = Secondary, // Custom green for focused border
-                      unfocusedBorderColor = Secondary, // Lighter green for unfocused border
-                      cursorColor = Secondary, // Custom green for the cursor
-                      focusedLabelColor = Secondary, // Custom green for focused label
-                      unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ))
           // Language Input Field
           OutlinedTextField(
               value = language,
@@ -207,47 +267,8 @@ fun AddToBookScreen(
                       cursorColor = Secondary, // Custom green for the cursor
                       focusedLabelColor = Secondary, // Custom green for focused label
                       unfocusedLabelColor = Secondary // Lighter color for unfocused label
-                      ))
-          // Save Button
-          Button(
-              colors =
-                  ButtonDefaults.buttonColors(
-                      containerColor = Primary, // Light green
-                      contentColor = BackGround),
-              onClick = {
-                // Check if title and ISBN are not blank (required fields)
-                if (title.isNotBlank() && isbn.isNotBlank() && selectedGenre != null) {
-                  // You can handle book object creation here (e.g., save the book)
-                  val book =
-                      createDataBook(
-                          context,
-                          repository.getNewUUID(),
-                          title,
-                          author,
-                          description,
-                          rating,
-                          photo,
-                          language,
-                          isbn,
-                          listOf(selectedGenre!!))
-                  if (book == null) {
-                    Toast.makeText(context, "Invalid argument", Toast.LENGTH_SHORT).show()
-                  } else {
-                    repository.addBook(book, OnSucess = {}, onFailure = {})
-                  }
-                } else {
-                  // Show a Toast message if title or ISBN is empty
-                  Toast.makeText(context, "Title and ISBN are required.", Toast.LENGTH_SHORT).show()
-                }
-              },
-              // Enable the button only if title and ISBN are filled
-              enabled = title.isNotBlank() && isbn.isNotBlank(),
-              modifier = Modifier.testTag("bookSave").testTag("Save")) {
-                // Text displayed on the button
-                Text("Save", modifier = Modifier.testTag("bookSave").testTag("Save"))
-              }
-        }
-      })
+                      ))*/
+
 }
 
 fun createDataBook(
