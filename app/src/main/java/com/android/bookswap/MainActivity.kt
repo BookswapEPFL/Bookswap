e4b5f700beea884ce2a13730590670a6fa9fd70e
TheoSchlaeppi <81753102+TheoSchlaeppi@users.noreply.github.com>
2024-11-14 20:59:02 +0100

    chore:KTFmt formatting

--- app/src/main/java/com/android/bookswap/MainActivity.kt
+++ app/src/main/java/com/android/bookswap/MainActivity.kt
@@ -115,7 +115,7 @@ class MainActivity : ComponentActivity() {
 
     val currentUserUUID = UUID.fromString("550e8400-e29b-41d4-a716-446655440000")
     val otherUserUUID = UUID.fromString("550e8400-e29b-41d4-a716-446655440001")
-     val testUserUUID = UUID.fromString("550e8400-e29b-41d4-a716-446655440002")
+    val testUserUUID = UUID.fromString("550e8400-e29b-41d4-a716-446655440002")
     val currentUserPlaceholder =
         DataUser(
             currentUserUUID,
