6fc353eea3c460e5c6aadef0e2c902f9e5e5c9db
LeniCadorinEpfl <leni.cadorin@epfl.ch>
2024-11-21 20:45:32 +0100

    fix: code formatting issue with ktfmt

--- app/src/main/java/com/android/bookswap/data/source/api/GoogleBookDataSource.kt
+++ app/src/main/java/com/android/bookswap/data/source/api/GoogleBookDataSource.kt
@@ -58,10 +58,10 @@ class GoogleBookDataSource(context: Context) {
       // we do not always use get..OrNull()
       val item = json.getJSONArray("items").getJSONObject(0).getJSONObject("volumeInfo")
 
-        val language =
-            item.getStringOrNull("language")?.lowercase()?.let { languageCode ->
-                BookLanguages.values().find { it.languageCode.equals(languageCode, ignoreCase = true) }
-            } ?: BookLanguages.OTHER
+      val language =
+          item.getStringOrNull("language")?.lowercase()?.let { languageCode ->
+            BookLanguages.values().find { it.languageCode.equals(languageCode, ignoreCase = true) }
+          } ?: BookLanguages.OTHER
 
       // We do not know where the ISBN_13 is, so we need to filter for it
       val industryIdentifiers = item.getJSONArray("industryIdentifiers")
