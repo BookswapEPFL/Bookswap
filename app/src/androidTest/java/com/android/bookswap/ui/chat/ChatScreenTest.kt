ec1139b70981b270e09a2bbca92521c0e8c4c447
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-11-22 18:08:58 +0100

    Refactor ChatScreen test tags
    Refactor test tags to use C.Tag.ChatScreen

--- app/src/androidTest/java/com/android/bookswap/ui/chat/ChatScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/chat/ChatScreenTest.kt
@@ -20,6 +20,7 @@ import com.android.bookswap.data.DataUser
 import com.android.bookswap.data.MessageType
 import com.android.bookswap.data.repository.MessageRepository
 import com.android.bookswap.data.repository.PhotoFirebaseStorageRepository
+import com.android.bookswap.resources.C
 import com.android.bookswap.ui.navigation.NavigationActions
 import com.android.bookswap.ui.theme.ColorVariable
 import com.google.firebase.firestore.ListenerRegistration
@@ -102,8 +103,8 @@ class ChatScreenTest {
           mockNavigationActions,
           photoStorage = mockPhotoStorage)
     }
-    composeTestRule.onNodeWithTag("message_input_field").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("send_button").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.message).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.confirm_button).assertIsDisplayed()
   }
 
   @Test
@@ -119,30 +120,31 @@ class ChatScreenTest {
           mockNavigationActions,
           photoStorage = mockPhotoStorage)
     }
-    composeTestRule.onNodeWithTag("message_input_field").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("send_button").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.message).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.confirm_button).assertIsDisplayed()
     placeHolderData.forEach { message ->
       if (message.uuid != imageTestMessageUUID) {
         composeTestRule.waitUntil {
           composeTestRule
-              .onAllNodesWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+              .onAllNodesWithTag(
+                  "${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
               .fetchSemanticsNodes()
               .isNotEmpty()
         }
         composeTestRule
-            .onNodeWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+            .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
             .assertIsDisplayed()
         composeTestRule
-            .onNodeWithTag("message_text ${message.uuid}", useUnmergedTree = true)
+            .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.content, useUnmergedTree = true)
             .assertIsDisplayed()
         composeTestRule
-            .onNodeWithTag("message_text ${message.uuid}", useUnmergedTree = true)
+            .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.content, useUnmergedTree = true)
             .assertTextEquals(message.text)
         composeTestRule
-            .onNodeWithTag("message_timestamp ${message.uuid}", useUnmergedTree = true)
+            .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.timestamp, useUnmergedTree = true)
             .assertIsDisplayed()
         composeTestRule
-            .onNodeWithTag("message_timestamp ${message.uuid}", useUnmergedTree = true)
+            .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.timestamp, useUnmergedTree = true)
             .assertTextEquals(formatTimestamp(message.timestamp))
       }
     }
@@ -163,11 +165,12 @@ class ChatScreenTest {
     }
 
     composeTestRule
-        .onNodeWithTag("column", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.scrollable, useUnmergedTree = true)
         .performScrollToIndex(mockMessageRepository.messages.size - 1)
 
     composeTestRule
-        .onNodeWithTag("hobbit", useUnmergedTree = true)
+        .onNodeWithTag(
+            "${imageTestMessageUUID}_" + C.Tag.ChatScreen.content, useUnmergedTree = true)
         .assertExists("The last message should be an image with the test tag 'hobbit'")
   }
 
@@ -181,7 +184,7 @@ class ChatScreenTest {
           mockNavigationActions,
           photoStorage = mockPhotoStorage)
     }
-    composeTestRule.onNodeWithTag("send_button").assertHasClickAction()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.confirm_button).assertHasClickAction()
   }
 
   @Test
@@ -195,8 +198,8 @@ class ChatScreenTest {
           photoStorage = mockPhotoStorage)
     }
     val testInput = "Hello, World!"
-    composeTestRule.onNodeWithTag("message_input_field").performTextInput(testInput)
-    composeTestRule.onNodeWithTag("message_input_field").assertTextEquals(testInput)
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.message).performTextInput(testInput)
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.message).assertTextEquals(testInput)
   }
 
   @Test
@@ -215,8 +218,8 @@ class ChatScreenTest {
 
     val testInput = "Hello, World!"
 
-    composeTestRule.onNodeWithTag("message_input_field").performTextInput(testInput)
-    composeTestRule.onNodeWithTag("send_button").performClick()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.message).performTextInput(testInput)
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.confirm_button).performClick()
 
     // Verify that the message was sent
     val sentMessage = mockMessageRepository.messages.find { it.uuid == testMessageId }
@@ -237,12 +240,14 @@ class ChatScreenTest {
           photoStorage = mockPhotoStorage)
     }
 
-    composeTestRule.onNodeWithTag("chatTopAppBar").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("chatName").assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.top_app_bar_container).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.TopAppBar.screen_title).assertIsDisplayed()
     composeTestRule
-        .onNodeWithTag("chatName")
+        .onNodeWithTag(C.Tag.TopAppBar.screen_title)
         .assertTextEquals(otherUser.firstName + " " + otherUser.lastName)
-    composeTestRule.onNodeWithTag("profileIcon", useUnmergedTree = true).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(C.Tag.TopAppBar.profile_button, useUnmergedTree = true)
+        .assertIsDisplayed()
   }
 
   @Test
@@ -260,27 +265,33 @@ class ChatScreenTest {
 
     val messageNode =
         composeTestRule.onNodeWithTag(
-            "message_item ${placeHolderData.first().uuid}", useUnmergedTree = true)
+            "${placeHolderData.first().uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
     messageNode.assertExists("Message item not found")
 
     messageNode.performSemanticsAction(SemanticsActions.OnLongClick)
 
     composeTestRule.waitUntil(timeoutMillis = 5000) {
       composeTestRule
-          .onAllNodesWithTag("editButton", useUnmergedTree = true)
+          .onAllNodesWithTag(C.Tag.ChatScreen.edit, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isNotEmpty() &&
           composeTestRule
-              .onAllNodesWithTag("deleteButton", useUnmergedTree = true)
+              .onAllNodesWithTag(C.Tag.ChatScreen.delete, useUnmergedTree = true)
               .fetchSemanticsNodes()
               .isNotEmpty()
     }
 
-    composeTestRule.onNodeWithTag("editButton", useUnmergedTree = true).assertIsDisplayed()
-    composeTestRule.onNodeWithTag("deleteButton", useUnmergedTree = true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.edit, useUnmergedTree = true).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.delete, useUnmergedTree = true)
+        .assertIsDisplayed()
 
-    composeTestRule.onNodeWithTag("editButton", useUnmergedTree = true).assertHasClickAction()
-    composeTestRule.onNodeWithTag("deleteButton", useUnmergedTree = true).assertHasClickAction()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.edit, useUnmergedTree = true)
+        .assertHasClickAction()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.delete, useUnmergedTree = true)
+        .assertHasClickAction()
   }
 
   @Test
@@ -299,32 +310,33 @@ class ChatScreenTest {
 
     val message = placeHolderData.first()
     val messageNode =
-        composeTestRule.onNodeWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+        composeTestRule.onNodeWithTag(
+            "${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
     messageNode.assertExists("Message item not found")
 
     messageNode.performSemanticsAction(SemanticsActions.OnLongClick)
 
     composeTestRule.waitUntil(timeoutMillis = 5000) {
       composeTestRule
-          .onAllNodesWithTag("deleteButton", useUnmergedTree = true)
+          .onAllNodesWithTag(C.Tag.ChatScreen.delete, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isNotEmpty()
     }
 
-    composeTestRule.onNodeWithTag("deleteButton", useUnmergedTree = true).performClick()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.delete, useUnmergedTree = true).performClick()
 
     val deletedMessage = mockMessageRepository.messages.find { it.uuid == message.uuid }
     assert(deletedMessage == null) { "Message was not deleted" }
 
     composeTestRule.waitUntil(timeoutMillis = 5000) {
       composeTestRule
-          .onAllNodesWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+          .onAllNodesWithTag("${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isEmpty()
     }
 
     composeTestRule
-        .onNodeWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+        .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
         .assertDoesNotExist()
   }
 
@@ -346,33 +358,36 @@ class ChatScreenTest {
     val newText = "Updated message text"
 
     val messageNode =
-        composeTestRule.onNodeWithTag("message_item ${message.uuid}", useUnmergedTree = true)
+        composeTestRule.onNodeWithTag(
+            "${message.uuid}_" + C.Tag.ChatScreen.messages, useUnmergedTree = true)
     messageNode.assertExists("Message item not found")
 
     messageNode.performSemanticsAction(SemanticsActions.OnLongClick)
 
-    composeTestRule.waitUntil(timeoutMillis = 5000) {
+    composeTestRule.waitUntil(timeoutMillis = 5367) {
       composeTestRule
-          .onAllNodesWithTag("editButton", useUnmergedTree = true)
+          .onAllNodesWithTag(C.Tag.ChatScreen.edit, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isNotEmpty()
     }
 
-    composeTestRule.onNodeWithTag("editButton", useUnmergedTree = true).performClick()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.edit, useUnmergedTree = true).performClick()
 
     composeTestRule
-        .onNodeWithTag("message_input_field", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.message, useUnmergedTree = true)
         .assertTextEquals(message.text)
 
     composeTestRule
-        .onNodeWithTag("message_input_field", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.message, useUnmergedTree = true)
         .performTextClearance()
     composeTestRule
-        .onNodeWithTag("message_input_field", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.message, useUnmergedTree = true)
         .performTextInput(newText)
-    composeTestRule.onNodeWithTag("send_button", useUnmergedTree = true).performClick()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.confirm_button, useUnmergedTree = true)
+        .performClick()
 
-    composeTestRule.waitUntil(timeoutMillis = 5000) {
+    composeTestRule.waitUntil(timeoutMillis = 5390) {
       mockMessageRepository.messages.find { it.uuid == message.uuid }?.text == newText
     }
 
@@ -381,9 +396,9 @@ class ChatScreenTest {
       "Message was not updated correctly"
     }
 
-    composeTestRule.waitUntil(timeoutMillis = 5000) {
+    composeTestRule.waitUntil(timeoutMillis = 5399) {
       composeTestRule
-          .onNodeWithTag("message_text ${message.uuid}", useUnmergedTree = true)
+          .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.content, useUnmergedTree = true)
           .fetchSemanticsNode()
           .config
           .getOrNull(SemanticsProperties.Text)
@@ -391,11 +406,11 @@ class ChatScreenTest {
     }
 
     composeTestRule
-        .onNodeWithTag("message_text ${message.uuid}", useUnmergedTree = true)
+        .onNodeWithTag("${message.uuid}_" + C.Tag.ChatScreen.content, useUnmergedTree = true)
         .assertTextEquals(newText)
 
     composeTestRule
-        .onNodeWithTag("message_input_field", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.message, useUnmergedTree = true)
         .assertTextEquals("")
   }
 
@@ -414,30 +429,35 @@ class ChatScreenTest {
     }
 
     composeTestRule
-        .onNodeWithTag("column", useUnmergedTree = true)
+        .onNodeWithTag(C.Tag.ChatScreen.scrollable, useUnmergedTree = true)
         .performScrollToIndex(mockMessageRepository.messages.size - 1)
 
     composeTestRule
-        .onNodeWithTag("message_item_column $imageTestMessageUUID", useUnmergedTree = true)
+        .onNodeWithTag(
+            "${imageTestMessageUUID}_" + C.Tag.ChatScreen.container, useUnmergedTree = true)
         .performClick()
 
     composeTestRule.waitUntil(timeoutMillis = 5000) {
       composeTestRule
-          .onAllNodesWithTag("popupImage", useUnmergedTree = true)
+          .onAllNodesWithTag(C.Tag.ChatScreen.pop_out, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isNotEmpty()
     }
-    composeTestRule.onNodeWithTag("popupImage", useUnmergedTree = true).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.pop_out, useUnmergedTree = true)
+        .assertIsDisplayed()
 
-    composeTestRule.onNodeWithTag("popupImage", useUnmergedTree = true).performClick()
+    composeTestRule.onNodeWithTag(C.Tag.ChatScreen.pop_out, useUnmergedTree = true).performClick()
 
     composeTestRule.waitUntil(timeoutMillis = 5000) {
       composeTestRule
-          .onAllNodesWithTag("popupImage", useUnmergedTree = true)
+          .onAllNodesWithTag(C.Tag.ChatScreen.pop_out, useUnmergedTree = true)
           .fetchSemanticsNodes()
           .isEmpty()
     }
-    composeTestRule.onNodeWithTag("popupImage", useUnmergedTree = true).assertDoesNotExist()
+    composeTestRule
+        .onNodeWithTag(C.Tag.ChatScreen.pop_out, useUnmergedTree = true)
+        .assertDoesNotExist()
   }
 
   @Test
