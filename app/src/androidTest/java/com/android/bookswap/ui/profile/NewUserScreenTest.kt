e09e7c9ac1bf81a8a3aea2c5f9fd0895c71eb9e7
Jaime Oliver Pastor <jaime.oliverpastor@epfl.ch>
2024-11-27 17:38:51 +0100

    fix: run ktfmtFormat

--- app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollTo
 import androidx.compose.ui.test.performTextInput
 import com.android.bookswap.model.UserViewModel
 import com.android.bookswap.resources.C
