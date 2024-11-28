cfd4cacd25aa585830a4e95cc502cca79b158175
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-11-28 18:43:18 +0100

    Fix data type in test

--- app/src/test/java/com/android/bookswap/model/chat/ContactViewModelTest.kt
+++ app/src/test/java/com/android/bookswap/model/chat/ContactViewModelTest.kt
@@ -72,8 +72,7 @@ class ContactViewModelTest {
       )
 
   // Mock user data for testing
-  val user =
-      DataUser(userUUID = listOfUUIDs[0], contactList = listOfUUIDs.drop(1).map { it.toString() })
+  val user = DataUser(userUUID = listOfUUIDs[0], contactList = listOfUUIDs.drop(1))
 
   // Mock users in the database (this simulates the users repository)
   val usersDB: List<DataUser> =
