7b19e161b5d7def6495fcf98253d31b4c6ebd0b6
Jaime Oliver Pastor <jaime.oliverpastor@epfl.ch>
2024-11-28 13:11:00 +0100

    fix: change test

--- app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
+++ app/src/androidTest/java/com/android/bookswap/ui/profile/NewUserScreenTest.kt
@@ -8,13 +8,11 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.test.espresso.intent.Intents
 import com.android.bookswap.model.UserViewModel
 import com.android.bookswap.resources.C
 import com.android.bookswap.ui.navigation.NavigationActions
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -29,11 +27,6 @@ class NewUserScreenTest {
     navigationActions = mockk(relaxed = true)
     userVM = mockk(relaxed = true)
     composeTestRule.setContent { NewUserScreen(navigationActions, userVM) }
-  }
-
-  @After
-  fun tearDown() {
-    Intents.release()
   }
 
   @Test
@@ -102,11 +95,11 @@ class NewUserScreenTest {
 
     composeTestRule.onNodeWithTag(C.Tag.NewUser.greeting).performTextInput("Mr.")
     composeTestRule.onNodeWithTag(C.Tag.NewUser.firstname).performTextInput("")
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.lastname).performTextInput("Doe")
+    composeTestRule.onNodeWithTag(C.Tag.NewUser.lastname).performTextInput("")
     composeTestRule
         .onNodeWithTag(C.Tag.NewUser.email)
         .performTextInput("john.doe.com") // Invalid email
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.phone).performTextInput("+4122345678")
+    composeTestRule.onNodeWithTag(C.Tag.NewUser.phone).performTextInput("784")
 
     composeTestRule.onNodeWithTag(C.Tag.NewUser.confirm).performClick()
 
@@ -117,28 +110,6 @@ class NewUserScreenTest {
         .assertExists()
         .assertIsDisplayed()
         .assertTextContains("Invalid email format")
-    composeTestRule
-        .onNodeWithTag(C.Tag.NewUser.firstname_error)
-        .assertExists()
-        .assertIsDisplayed()
-        .assertTextContains("First name required")
-  }
-
-  @Test
-  fun clickOnCreateButtonWithEmptyFieldsShowsErrors() {
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.confirm).performClick()
-
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.greeting).performTextInput("")
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.firstname).performTextInput("")
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.lastname).performTextInput("")
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.email).performTextInput("notanemail")
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.phone).performTextInput("")
-
-    composeTestRule.onNodeWithTag(C.Tag.NewUser.confirm).performClick()
-
-    verify(exactly = 0) { navigationActions.navigateTo(C.Route.MAP) }
-
     composeTestRule
         .onNodeWithTag(C.Tag.NewUser.firstname_error)
         .assertExists()
@@ -150,11 +121,5 @@ class NewUserScreenTest {
         .assertExists()
         .assertIsDisplayed()
         .assertTextContains("Last name required")
-
-    composeTestRule
-        .onNodeWithTag(C.Tag.NewUser.email_error)
-        .assertExists()
-        .assertIsDisplayed()
-        .assertTextContains("Invalid email format")
   }
 }
