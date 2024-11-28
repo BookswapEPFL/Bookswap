193235fb4042bd7de18a359233a83b5462d1de6d
EdenKahane <kahaneeden@protonmail.com>
2024-11-28 01:42:05 +0100

    fix: fix format

--- app/src/test/java/com/android/bookswap/model/AppConfigTest.kt
+++ app/src/test/java/com/android/bookswap/model/AppConfigTest.kt
@@ -6,13 +6,11 @@ import org.junit.Test
 
 class AppConfigTest {
 
-    @Test
-    fun `assert creation of data class`() {
-        val mockUserViewModel: UserViewModel = mockk()
-        val appConfig = AppConfig(
-            userViewModel = mockUserViewModel
-        )
+  @Test
+  fun `assert creation of data class`() {
+    val mockUserViewModel: UserViewModel = mockk()
+    val appConfig = AppConfig(userViewModel = mockUserViewModel)
 
-        assertEquals(mockUserViewModel, appConfig.userViewModel)
-    }
+    assertEquals(mockUserViewModel, appConfig.userViewModel)
+  }
 }
