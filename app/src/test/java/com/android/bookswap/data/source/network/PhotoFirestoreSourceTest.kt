6fe3e8c7f8f4e6c5912520e37e1f0d17324214ff
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-12-03 16:29:36 +0100

    Fix tests

--- app/src/test/java/com/android/bookswap/data/source/network/PhotoFirestoreSourceTest.kt
+++ app/src/test/java/com/android/bookswap/data/source/network/PhotoFirestoreSourceTest.kt
@@ -55,7 +55,7 @@ class PhotoFirestoreSourceTest {
     // Arrange snapshot
     every { mockDocumentReference.get() } returns Tasks.forResult(mockDocumentSnapshot)
     every { mockDocumentSnapshot.get("uuid") } returns testPhoto.uuid.toString()
-    every { mockDocumentSnapshot.getString("url") } returns testPhoto.url
+    every { mockDocumentSnapshot.get("url") } returns testPhoto.url.toString()
     every { mockDocumentSnapshot.get("timestamp") } returns testPhoto.timestamp
     every { mockDocumentSnapshot.getString("base64") } returns testPhoto.base64
   }
