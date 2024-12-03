58575346f1d7a5ad8bdf4db6e25064524c01d8c1
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2024-12-03 13:34:58 +0100

    Fix UUID parsing/convert and add Long parsing/convert

--- app/src/main/java/com/android/bookswap/data/source/network/DataConverter.kt
+++ app/src/main/java/com/android/bookswap/data/source/network/DataConverter.kt
@@ -17,12 +17,12 @@ object DataConverter {
 
   private fun parse_bits_UUID(string: String): UUID? {
     val uid =
-        string.removeSurrounding("{", "}").let { sid ->
-          buildMap<String, Long> {
+        string.removePrefix("{").removeSuffix( "}").let { sid ->
+          buildMap<String, Long?> {
             sid.split(", ").let { comps -> // List<String> ["lsb=Long", "msb=Long"]
               comps.map { half -> // String "name=Long"
                 half.split("=").let { part -> // List<String> ["name", "Long"]
-                  put(part.first(), part.last().toLong())
+                  put(part.first(), parse_raw_long(part.last()))
                 }
               }
             }
@@ -47,7 +47,40 @@ object DataConverter {
     }
   }
 
-  fun parse_raw_UUID_list(string: String) {
-    var temp_string = string.removeSurrounding("[", "]")
+  fun parse_raw_UUID_list(string: String): List<UUID?> {
+    val temp_string = string.removeSurrounding("[", "]").removeSurrounding("{", "}")
+	val delimiter = if(temp_string.contains("=")){"}, "}else{", "}
+	return temp_string.split(delimiter).map{ parse_raw_UUID(it) }
+  }
+  
+  fun parse_raw_long(string: String): Long? {
+	return try {
+		string.toLong()
+	} catch (e: Exception) {
+	  Log.e(
+		"BookSwap_FirestoreDataConverter",
+		"DataConverter.parse_raw_long: Error converting \"$string\" to Long, the provided string cannot be parsed as a Long")
+	  null
+	}
+  }
+  
+  fun parse_raw_long_list(string: String): List<Long?> {
+	return string.split(", ").map{parse_raw_long(it)}
+  }
+  
+  fun convert_UUID(uuid: UUID): String {
+	return uuid.toString()
+  }
+  
+  fun convert_UUID_list(uuid_list: List<UUID>): List<String> {
+	return uuid_list.map { it.toString() }
+  }
+  
+  fun convert_Long(long: Long): String {
+	return long.toString()
+  }
+  
+  fun convert_Long_list(long_list: List<Long>): List<String> {
+	return long_list.map { it.toString() }
   }
 }
