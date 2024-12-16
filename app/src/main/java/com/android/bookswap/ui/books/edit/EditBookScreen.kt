ee5c1a9f10672fee192558c153d9b3c589c00f1a
chockencore <sphillot@netplus.ch>
2024-12-16 10:00:44 +0100

    chore: ktfmt format

--- app/src/main/java/com/android/bookswap/ui/books/edit/EditBookScreen.kt
+++ app/src/main/java/com/android/bookswap/ui/books/edit/EditBookScreen.kt
@@ -5,16 +5,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -31,7 +25,6 @@ import com.android.bookswap.model.edit.EditBookViewModel
 import com.android.bookswap.resources.C
 import com.android.bookswap.ui.components.ButtonComponent
 import com.android.bookswap.ui.components.EntriesListBookComponent
-import com.android.bookswap.ui.navigation.NavigationActions
 import com.android.bookswap.ui.theme.ColorVariable
 import java.util.UUID
 
@@ -126,7 +119,7 @@ fun EditBookScreen(
                         }) {
                           Text("Save")
                         }
-                      Spacer(modifier = Modifier.fillMaxWidth(SPACE_BETWEEN_BUTTON))
+                    Spacer(modifier = Modifier.fillMaxWidth(SPACE_BETWEEN_BUTTON))
                     ButtonComponent(
                         modifier =
                             Modifier.testTag(C.Tag.BookEntryComp.cancel_button).fillMaxWidth(),
