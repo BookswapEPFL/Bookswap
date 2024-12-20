c6e67946697693410fc2d0bf4d8e7863f5c9138c
EdenKahane <kahaneeden@protonmail.com>
2024-12-20 02:08:21 +0100

    feature: create NotificationService

---
+++ app/src/main/java/com/android/bookswap/model/NotificationService.kt
@@ -0,0 +1,28 @@
+package com.android.bookswap.model
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.android.bookswap.BookSwapApplication
+import com.android.bookswap.R
+import kotlin.random.Random
+
+class NotificationService(private val context: Context) {
+    private val notificationManager=context.getSystemService(NotificationManager::class.java)
+
+    fun sendNotification(title: String, messageBody: String){
+        val notification= NotificationCompat.Builder(context, BookSwapApplication.NOTIFICATION_CHANNEL)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setSmallIcon(R.drawable.logo5)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(
+            Random.nextInt(),
+            notification
+        )
+    }
+
+}
